0c8d4d1a8457550db462c19af75e7859348e415c
acidmikk <sv.acid6@gmail.com>
2021-11-19 01:05:18 +0300

    collect

---
+++ tex_prog/tex_prog/DockCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsShip
+{
+    class DockCollection
+    {
+        readonly Dictionary<string, Parking<Vehicle>> parkingStages;
+        public List<string> Keys => parkingStages.Keys.ToList();
+        private readonly int pictureWidth;
+        private readonly int pictureHeight;
+        public DockCollection(int pictureWidth, int pictureHeight)
+        {
+            parkingStages = new Dictionary<string, Parking<Vehicle>>();
+            this.pictureWidth = pictureWidth;
+            this.pictureHeight = pictureHeight;
+        }
+    }
+}
