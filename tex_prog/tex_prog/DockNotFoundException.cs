6df4d650286c774d6d5d96d9b7ef9d0d3abf35cf
acidmikk <sv.acid6@gmail.com>
2022-03-03 14:11:33 +0400

    pre-done

---
+++ tex_prog/tex_prog/DockNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsShip
+{
+    internal class DockNotFoundException : Exception
+    {
+        public DockNotFoundException(int i) : base("Не найден корабль по месту " + i)
+        { }
+    }
+}
---
+++ tex_prog/tex_prog/DockOverflowException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsShip
+{
+    internal class DockOverflowException : Exception
+    {
+        public DockOverflowException() : base("В доке нет места!")
+        { }
+    }
+}
